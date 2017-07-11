28ae6a7b50d32e64569e8ac05ceb6a4ee32a8398
Alvin <alvin.lamartiniere@gmail.com>
2017-07-12 00:56:10 +0200

    Version stable de l'appli + button share twitter

--- js/api.js
+++ js/api.js
@@ -1,4 +1,95 @@
 var API_URL = 'https://haag4kee92.execute-api.eu-west-2.amazonaws.com/prod';
+var all_arguments = [];
+
+var fun_elapsed = function(timestamp){
+	var startTime = timestamp, endTime = Date.now();
+
+	var timeDiff = endTime/1000 - startTime; //in s
+
+
+	// get seconds
+	var seconds = Math.round(timeDiff);
+	if (seconds < 60) {
+		return {unit:"seconds", value : seconds};
+	}
+
+	var minutes = Math.round((seconds/60));
+	if (minutes < 60) {
+		return {unit:"minutes", value : minutes};
+	}
+
+	var hours = Math.round((minutes/60));
+	if (hours < 24) {
+		return {unit:"hours", value : hours};
+	}
+
+	var days = Math.round((hours/24));
+	if (days < 30) {
+		return {unit:"days", value : days};
+	}
+
+	var weeks = Math.round((days/7));
+	if (weeks < 4) {
+		return {unit:"weeks", value : weeks};
+	}
+
+	var months = Math.round((weeks/4));
+	if (months < 12) {
+		return {unit:"months", value : months};
+	}
+
+	var years = Math.round((months/4));
+	if (years < 12) {
+		return {unit:"years", value : years};
+	}
+	
+};
+
+var display_args = function(destination, callback, data, filter_or_sort){
+	$(destination).html('');
+
+	if (filter_or_sort=="sort"){
+		data.sort(callback);
+	} 
+	else if (filter_or_sort=="filter"){
+		data.filter(callback);
+	}
+	
+	if (data.length>0) {
+		data.some(function(argumentItem) {
+			var $this = argumentItem;
+			
+
+			
+			var elapsed = fun_elapsed(parseInt(argumentItem.ArgumentTimestamp));
+			
+	        $(destination).append(
+		  		'<div class="card p-3 lazy" data-argumentUUID="' + argumentItem.Keyworduuid + '" data-argumentTimeStamp="' + argumentItem.ArgumentTimestamp + '">' +
+				    '<div class="card-block">' +
+				      '<h4 class="card-title">' + argumentItem.Keyword.Keyword + '</h4>' +
+				      '<a href="https://twitter.com/share?hashtags=sellmethis&text=Sell me this : ' + argumentItem.Keyword.Keyword + '!&url=https://s3.eu-west-2.amazonaws.com/bootstrapsmt/index.html?q=' + argumentItem.Keyword.Keyword + '" class="btn btn-primary" data-size="large" data-text="Sell me this!" data-url="https://s3.eu-west-2.amazonaws.com/bootstrapsmt/index.html" data-hashtags="SellMeThis" data-show-count="false"><i class="fa fa-twitter"></i></a>' +
+				      '<p class="card-text">' + argumentItem.Text + '</p>' +
+				      '<p class="card-text"><small class="text-muted">Submitted '+ elapsed.value +' '+ elapsed.unit +' ago</small></p>' +
+				      '<div class="btn-group thumbs" data-toggle="buttons">' +
+				      	'<label class="btn btn-primary active" aria-pressed="true">' +
+					      '<input type="radio" name="options" data-rate="Up" data-keyid="' + argumentItem.Keyworduuid + '" data-argid="' + argumentItem.ArgumentTimestamp + '" data-upvote autocomplete="off"><i class="fa fa-thumbs-up" aria-hidden="true"></i> <span data-argid="' + argumentItem.ArgumentTimestamp + '" class="upspan">' + argumentItem.Up +'</span>'+
+					    '</label>' +
+					    '<label class="btn btn-primary active" aria-pressed="true">' +
+					      '<input type="radio" name="options" data-rate="Down" data-keyid="' + argumentItem.Keyworduuid + '" data-argid="' + argumentItem.ArgumentTimestamp + '" data-downvote autocomplete="off"><i class="fa fa-thumbs-down" aria-hidden="true"></i> <span data-argid="' + argumentItem.ArgumentTimestamp + '" class="downspan">' + argumentItem.Down +'</span>'+
+					    '</label>' +
+				      '</div>' +
+				    '</div>' +
+				  '</div>' 
+
+		  	);
+		  
+		});
+	} 
+	else {
+		$(destination).append('<div><h4 class="card-title">No results</h4></div>');
+		
+	}
+};
 
 $(document).ready(function(){
 	$.ajax({
@@ -50,10 +141,51 @@ $(document).ready(function(){
 				var $this = keywordItem;
 				$('#keywords-list').append(
 					'<div class="card p-3">' +
-						    '<span>' + $this.Keyword + '</span>' +
+						    '<span><a href data-search="'+$this.Keyword+'">' + $this.Keyword + '</a></span>' +
 						  '</div>' 
 
 						   );
+				$("#keywords-list a").click(function( event ) {
+					event.preventDefault();
+					$("#search-input").val($(this).data("search"));
+					$('#searchresults').show();
+					$('html, body').animate({
+				        scrollTop: $("#searchresults").offset().top
+				    }, 1000);
+				    var q_term = $('#search-input').val();
+				    var result_args = all_arguments.filter(function(argumentItem){
+				    	return argumentItem.Keyword.Keyword.toLowerCase().startsWith(q_term.toLowerCase());
+				    });
+
+				    display_args('#search-latest-arguments',
+								function(arg1, arg2){return arg2.ArgumentTimestamp-arg1.ArgumentTimestamp},
+								result_args,
+								"sort" );
+
+				    display_args('#search-best-ratings',
+								function(arg1, arg2){return arg2.Up-arg1.Up},
+								result_args,
+								"sort" );
+				    $('input[data-rate]').change(function() {
+						$.ajax({
+							type: 'POST',
+							url: API_URL + '/vote',
+							data: JSON.stringify({
+													"Keyword_id": $(this).data('keyid'), 
+													"Timestamp": $(this).data('argid'),
+													"Rate":$(this).data('rate')
+												}),
+							contentType:"application/json",
+
+							success: function(data){
+								$('span[data-argid='+data.Argid+'][class=upspan]').html(data.Up);
+								$('span[data-argid='+data.Argid+'][class=downspan]').html(data.Down);
+							}
+						});
+
+						return false;
+					});
+				});
 			    
 			});
 
@@ -64,154 +196,20 @@ $(document).ready(function(){
 		type: 'GET',
 		url: API_URL + '/arguments',
 		success: function(data){
+			all_arguments = data.Items;
 
-			$('#latest-arguments').html('');
-			var mainKeyword = data.Keyword;
+			display_args('#latest-arguments',
+						function(arg1, arg2){return arg2.ArgumentTimestamp-arg1.ArgumentTimestamp},
+						all_arguments,
+						"sort" );
 
-			data.Items.sort(function(arg1, arg2){return arg2.ArgumentTimestamp-arg1.ArgumentTimestamp});
+			display_args('#best-ratings',
+						function(arg1, arg2){return arg2.Up-arg1.Up},
+						all_arguments,
+						"sort" );
 
-			data.Items.some(function(argumentItem) {
-				var $this = argumentItem;
-				function elapsed(timestamp){
-					var startTime = timestamp, endTime = Date.now();
-
-					var timeDiff = endTime/1000 - startTime; //in s
-
-
-					// get seconds
-					var seconds = Math.round(timeDiff);
-					if (seconds < 60) {
-						return {unit:"seconds", value : seconds};
-					}
-
-					var minutes = Math.round((seconds/60));
-					if (minutes < 60) {
-						return {unit:"minutes", value : minutes};
-					}
-
-					var hours = Math.round((minutes/60));
-					if (hours < 24) {
-						return {unit:"hours", value : hours};
-					}
-
-					var days = Math.round((hours/24));
-					if (days < 30) {
-						return {unit:"days", value : days};
-					}
-
-					var weeks = Math.round((days/7));
-					if (weeks < 4) {
-						return {unit:"weeks", value : weeks};
-					}
-
-					var months = Math.round((weeks/4));
-					if (months < 12) {
-						return {unit:"months", value : months};
-					}
-
-					var years = Math.round((months/4));
-					if (years < 12) {
-						return {unit:"years", value : years};
-					}
-					
-				}
-
-				
-				var elapsed = elapsed(parseInt(argumentItem.ArgumentTimestamp));
-				
-		        $('#latest-arguments').append(
-			  		'<div class="card p-3 lazy" data-argumentUUID="' + argumentItem.Keyworduuid + '" data-argumentTimeStamp="' + argumentItem.ArgumentTimestamp + '">' +
-					    '<div class="card-block">' +
-					      '<h4 class="card-title">' + argumentItem.Keyword.Keyword + '</h4>' +
-					      '<p class="card-text">' + argumentItem.Text + '</p>' +
-					      '<p class="card-text"><small class="text-muted">Submitted '+ elapsed.value +' '+ elapsed.unit +' ago</small></p>' +
-					      '<div class="btn-group thumbs" data-toggle="buttons">' +
-					      	'<label class="btn btn-primary active" aria-pressed="true">' +
-						      '<input type="radio" name="options" data-rate="Up" data-keyid="' + argumentItem.Keyworduuid + '" data-argid="' + argumentItem.ArgumentTimestamp + '" data-upvote autocomplete="off"><i class="fa fa-thumbs-up" aria-hidden="true"></i> <span data-argid="' + argumentItem.ArgumentTimestamp + '" class="upspan">' + argumentItem.Up +'</span>'+
-						    '</label>' +
-						    '<label class="btn btn-primary active" aria-pressed="true">' +
-						      '<input type="radio" name="options" data-rate="Down" data-keyid="' + argumentItem.Keyworduuid + '" data-argid="' + argumentItem.ArgumentTimestamp + '" data-downvote autocomplete="off"><i class="fa fa-thumbs-down" aria-hidden="true"></i> <span data-argid="' + argumentItem.ArgumentTimestamp + '" class="downspan">' + argumentItem.Down +'</span>'+
-						    '</label>' +
-					      '</div>' +
-					    '</div>' +
-					  '</div>' 
-
-			  	);
-			  
-			});
-
-			$('#best-ratings').html('');
-			data.Items.sort(function(arg1, arg2){return arg2.Up-arg1.Up});
 
 			
-	        data.Items.some(function(argumentItem) {
-				var $this = argumentItem;
-
-				function elapsed(timestamp){
-					var startTime = timestamp, endTime = Date.now();
-
-					var timeDiff = endTime/1000 - startTime; //in s
-
-
-					// get seconds
-					var seconds = Math.round(timeDiff);
-					if (seconds < 60) {
-						return {unit:"seconds", value : seconds};
-					}
-
-					var minutes = Math.round((seconds/60));
-					if (minutes < 60) {
-						return {unit:"minutes", value : minutes};
-					}
-
-					var hours = Math.round((minutes/60));
-					if (hours < 24) {
-						return {unit:"hours", value : hours};
-					}
-
-					var days = Math.round((hours/24));
-					if (days < 30) {
-						return {unit:"days", value : days};
-					}
-
-					var weeks = Math.round((days/7));
-					if (weeks < 4) {
-						return {unit:"weeks", value : weeks};
-					}
-
-					var months = Math.round((weeks/4));
-					if (months < 12) {
-						return {unit:"months", value : months};
-					}
-
-					var years = Math.round((months/4));
-					if (years < 12) {
-						return {unit:"years", value : years};
-					}
-					
-				}
-				var elapsed = elapsed(parseInt(argumentItem.ArgumentTimestamp));
-				
-			        $('#best-ratings').append(
-				  		'<div class="card p-3" data-argumentUUID="' + argumentItem.Keyworduuid + '" data-argumentTimeStamp="' + argumentItem.ArgumentTimestamp + '">' +
-						    '<div class="card-block">' +
-						      '<h4 class="card-title">' + argumentItem.Keyword.Keyword + '</h4>' +
-						      '<p class="card-text">' + argumentItem.Text + '</p>' +
-						      '<p class="card-text"><small class="text-muted">Submitted '+ elapsed.value +' '+ elapsed.unit +' ago</small></p>' +
-						      '<div class="btn-group thumbs" data-toggle="buttons">' +
-						      	'<label class="btn btn-primary active" aria-pressed="true">' +
-							      '<input type="radio" name="options" data-rate="Up" data-keyid="' + argumentItem.Keyworduuid + '" data-argid="' + argumentItem.ArgumentTimestamp + '" data-upvote autocomplete="off"><i class="fa fa-thumbs-up" aria-hidden="true"></i> <span data-argid="' + argumentItem.ArgumentTimestamp + '" class="upspan">' + argumentItem.Up +'</span>'+
-							    '</label>' +
-							    '<label class="btn btn-primary active" aria-pressed="true">' +
-							      '<input type="radio" name="options" data-rate="Down" data-keyid="' + argumentItem.Keyworduuid + '" data-argid="' + argumentItem.ArgumentTimestamp + '" data-downvote autocomplete="off"><i class="fa fa-thumbs-down" aria-hidden="true"></i> <span data-argid="' + argumentItem.ArgumentTimestamp + '" class="downspan">' + argumentItem.Down +'</span>'+
-							    '</label>' +
-						      '</div>' +
-						    '</div>' +
-						  '</div>' 
-
-				  	);
-			  
-			});
 
 
 			$('input[data-rate]').change(function() {
@@ -267,6 +265,52 @@ $('#carouselContent').bind('slid.bs.carousel', function (e) {
 $('.carousel').carousel({
     interval: false
 }); 
+
+$('#search-input').on('input', function() {
+	$('#searchresults').show();
+	$('html, body').animate({
+        scrollTop: $("#searchresults").offset().top
+    }, 1000);
+    var q_term = $('#search-input').val();
+    var result_args = all_arguments.filter(function(argumentItem){
+    	return argumentItem.Keyword.Keyword.toLowerCase().includes(q_term.toLowerCase());
+    });
+
+    display_args('#search-latest-arguments',
+				function(arg1, arg2){return arg2.ArgumentTimestamp-arg1.ArgumentTimestamp},
+				result_args,
+				"sort" );
+
+    display_args('#search-best-ratings',
+				function(arg1, arg2){return arg2.Up-arg1.Up},
+				result_args,
+				"sort" );
+    $('input[data-rate]').change(function() {
+		$.ajax({
+			type: 'POST',
+			url: API_URL + '/vote',
+			data: JSON.stringify({
+									"Keyword_id": $(this).data('keyid'), 
+									"Timestamp": $(this).data('argid'),
+									"Rate":$(this).data('rate')
+								}),
+			contentType:"application/json",
+
+			success: function(data){
+				$('span[data-argid='+data.Argid+'][class=upspan]').html(data.Up);
+				$('span[data-argid='+data.Argid+'][class=downspan]').html(data.Down);
+			}
+		});
+
+		return false;
+	});
+
+});
+
+$('#navbarResponsive form').on('submit', function(event){
+
+	event.preventDefault();
+});
 
 
 
